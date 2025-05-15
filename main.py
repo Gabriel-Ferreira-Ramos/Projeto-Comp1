43b30511a858a46099533085dd610082a646b1a9
Gabriel Ramos <gabrielframos12.06>
2025-05-15 09:50:53 -0300

    commit inicial

---
+++ main.py
@@ -0,0 +1,370 @@
+tabela_periodica = {
+    "H": {
+"nome": "Hidrogênio",
+"massa_atomica": 1.008,
+"numero_protons": 1,
+"numero_eletrons": 1,
+"simbolo": "H",
+"numero_atomico": 1,
+"grupo": 1,
+"periodo": 1
+},
+"He": {
+"nome": "Hélio",
+"massa_atomica": 4.0026,
+"numero_protons": 2,
+"numero_eletrons": 2,
+"simbolo": "He",
+"numero_atomico": 2,
+"grupo": 18,
+"periodo": 1
+},
+"Li": {
+"nome": "Lítio",
+"massa_atomica": 6.94,
+"numero_protons": 3,
+"numero_eletrons": 3,
+"simbolo": "Li",
+"numero_atomico": 3,
+"grupo": 1,
+"periodo": 2
+},
+"Be": {
+"nome": "Berílio",
+"massa_atomica": 9.0122,
+"numero_protons": 4,
+"numero_eletrons": 4,
+"simbolo": "Be",
+"numero_atomico": 4,
+"grupo": 2,
+"periodo": 2
+},
+"B": {
+"nome": "Boro",
+"massa_atomica": 10.81,
+"numero_protons": 5,
+"numero_eletrons": 5,
+"simbolo": "B",
+"numero_atomico": 5,
+"grupo": 13,
+"periodo": 2
+},
+"C": {
+"nome": "Carbono",
+"massa_atomica": 12.011,
+"numero_protons": 6,
+"numero_eletrons": 6,
+"simbolo": "C",
+"numero_atomico": 6,
+"grupo": 14,
+"periodo": 2
+},
+"N": {
+"nome": "Nitrogênio",
+"massa_atomica": 14.007,
+"numero_protons": 7,
+"numero_eletrons": 7,
+"simbolo": "N",
+"numero_atomico": 7,
+"grupo": 15,
+"periodo": 2
+},
+"O": {
+"nome": "Oxigênio",
+"massa_atomica": 15.999,
+"numero_protons": 8,
+"numero_eletrons": 8,
+"simbolo": "O",
+"numero_atomico": 8,
+"grupo": 16,
+"periodo": 2
+},
+"F": {
+"nome": "Flúor",
+"massa_atomica": 18.998,
+"numero_protons": 9,
+"numero_eletrons": 9,
+"simbolo": "F",
+"numero_atomico": 9,
+"grupo": 17,
+"periodo": 2
+},
+"Ne": {
+"nome": "Neônio",
+"massa_atomica": 20.18,
+"numero_protons": 10,
+"numero_eletrons": 10,
+"simbolo": "Ne",
+"numero_atomico": 10,
+"grupo": 18,
+"periodo": 2
+},
+"Np": {
+"nome": "Elemento93",
+"massa_atomica": 146.5,
+"numero_protons": 93,
+"numero_eletrons": 93,
+"simbolo": "Np",
+"numero_atomico": 93,
+"grupo": 4,
+"periodo": 6
+},
+"Pu": {
+"nome": "Elemento94",
+"massa_atomica": 147.0,
+"numero_protons": 94,
+"numero_eletrons": 94,
+"simbolo": "Pu",
+"numero_atomico": 94,
+"grupo": 5,
+"periodo": 6
+},
+"Am": {
+"nome": "Elemento95",
+"massa_atomica": 147.5,
+"numero_protons": 95,
+"numero_eletrons": 95,
+"simbolo": "Am",
+"numero_atomico": 95,
+"grupo": 6,
+"periodo": 6
+},
+"Cm": {
+"nome": "Elemento96",
+"massa_atomica": 148.0,
+"numero_protons": 96,
+"numero_eletrons": 96,
+"simbolo": "Cm",
+"numero_atomico": 96,
+"grupo": 7,
+"periodo": 6
+},
+"Bk": {
+"nome": "Elemento97",
+"massa_atomica": 148.5,
+"numero_protons": 97,
+"numero_eletrons": 97,
+"simbolo": "Bk",
+"numero_atomico": 97,
+"grupo": 8,
+"periodo": 6
+},
+"Cf": {
+"nome": "Elemento98",
+"massa_atomica": 149.0,
+"numero_protons": 98,
+"numero_eletrons": 98,
+"simbolo": "Cf",
+"numero_atomico": 98,
+"grupo": 9,
+"periodo": 6
+},
+"Es": {
+"nome": "Elemento99",
+"massa_atomica": 149.5,
+"numero_protons": 99,
+"numero_eletrons": 99,
+"simbolo": "Es",
+"numero_atomico": 99,
+"grupo": 10,
+"periodo": 6
+},
+"Fm": {
+"nome": "Elemento100",
+"massa_atomica": 150.0,
+"numero_protons": 100,
+"numero_eletrons": 100,
+"simbolo": "Fm",
+"numero_atomico": 100,
+"grupo": 11,
+"periodo": 6
+},
+"Md": {
+"nome": "Elemento101",
+"massa_atomica": 150.5,
+"numero_protons": 101,
+"numero_eletrons": 101,
+"simbolo": "Md",
+"numero_atomico": 101,
+"grupo": 12,
+"periodo": 6
+},
+"No": {
+"nome": "Elemento102",
+"massa_atomica": 151.0,
+"numero_protons": 102,
+"numero_eletrons": 102,
+"simbolo": "No",
+"numero_atomico": 102,
+"grupo": 13,
+"periodo": 6
+},
+"Lr": {
+"nome": "Elemento103",
+"massa_atomica": 151.5,
+"numero_protons": 103,
+"numero_eletrons": 103,
+"simbolo": "Lr",
+"numero_atomico": 103,
+"grupo": 14,
+"periodo": 6
+},
+"Rf": {
+"nome": "Elemento104",
+"massa_atomica": 152.0,
+"numero_protons": 104,
+"numero_eletrons": 104,
+"simbolo": "Rf",
+"numero_atomico": 104,
+"grupo": 15,
+"periodo": 6
+},
+"Db": {
+"nome": "Elemento105",
+"massa_atomica": 152.5,
+"numero_protons": 105,
+"numero_eletrons": 105,
+"simbolo": "Db",
+"numero_atomico": 105,
+"grupo": 16,
+"periodo": 6
+},
+"Sg": {
+"nome": "Elemento106",
+"massa_atomica": 153.0,
+"numero_protons": 106,
+"numero_eletrons": 106,
+"simbolo": "Sg",
+"numero_atomico": 106,
+"grupo": 17,
+"periodo": 6
+},
+"Bh": {
+"nome": "Elemento107",
+"massa_atomica": 153.5,
+"numero_protons": 107,
+"numero_eletrons": 107,
+"simbolo": "Bh",
+"numero_atomico": 107,
+"grupo": 18,
+"periodo": 6
+},
+"Hs": {
+"nome": "Elemento108",
+"massa_atomica": 154.0,
+"numero_protons": 108,
+"numero_eletrons": 108,
+"simbolo": "Hs",
+"numero_atomico": 108,
+"grupo": 1,
+"periodo": 7
+},
+"Mt": {
+"nome": "Elemento109",
+"massa_atomica": 154.5,
+"numero_protons": 109,
+"numero_eletrons": 109,
+"simbolo": "Mt",
+"numero_atomico": 109,
+"grupo": 2,
+"periodo": 7
+},
+"Ds": {
+"nome": "Elemento110",
+"massa_atomica": 155.0,
+"numero_protons": 110,
+"numero_eletrons": 110,
+"simbolo": "Ds",
+"numero_atomico": 110,
+"grupo": 3,
+"periodo": 7
+},
+"Rg": {
+"nome": "Elemento111",
+"massa_atomica": 155.5,
+"numero_protons": 111,
+"numero_eletrons": 111,
+"simbolo": "Rg",
+"numero_atomico": 111,
+"grupo": 4,
+"periodo": 7
+},
+"Cn": {
+"nome": "Elemento112",
+"massa_atomica": 156.0,
+"numero_protons": 112,
+"numero_eletrons": 112,
+"simbolo": "Cn",
+"numero_atomico": 112,
+"grupo": 5,
+"periodo": 7
+},
+"Nh": {
+"nome": "Elemento113",
+"massa_atomica": 156.5,
+"numero_protons": 113,
+"numero_eletrons": 113,
+"simbolo": "Nh",
+"numero_atomico": 113,
+"grupo": 6,
+"periodo": 7
+},
+"Fl": {
+"nome": "Elemento114",
+"massa_atomica": 157.0,
+"numero_protons": 114,
+"numero_eletrons": 114,
+"simbolo": "Fl",
+"numero_atomico": 114,
+"grupo": 7,
+"periodo": 7
+},
+"Mc": {
+"nome": "Elemento115",
+"massa_atomica": 157.5,
+"numero_protons": 115,
+"numero_eletrons": 115,
+"simbolo": "Mc",
+"numero_atomico": 115,
+"grupo": 8,
+"periodo": 7
+},
+"Lv": {
+"nome": "Elemento116",
+"massa_atomica": 158.0,
+"numero_protons": 116,
+"numero_eletrons": 116,
+"simbolo": "Lv",
+"numero_atomico": 116,
+"grupo": 9,
+"periodo": 7
+},
+"Ts": {
+"nome": "Elemento117",
+"massa_atomica": 158.5,
+"numero_protons": 117,
+"numero_eletrons": 117,
+"simbolo": "Ts",
+"numero_atomico": 117,
+"grupo": 10,
+"periodo": 7
+},
+"Og": {
+"nome": "Elemento118",
+"massa_atomica": 159.0,
+"numero_protons": 118,
+"numero_eletrons": 118,
+"simbolo": "Og",
+"numero_atomico": 118,
+"grupo": 11,
+"periodo": 7
+},
+}
+
+def main():
+
+    print('Ola Mundo!')
+
+
+
+    main()
